a9105b0f55b0a846cc59a32d492ec32b54b4edbc
nonadev <wesley.vieira.lima.1999@gmail.com>
2020-08-19 22:59:41 -0300

    jogo finalizado

--- scripts/Monster.gd
+++ scripts/Monster.gd
@@ -1,37 +1,50 @@
 extends StaticBody2D
 
-export(float) var DISTANCE = 115;
-export(bool) var WALK = true;
-export(bool) var FLIP_SPRITE = false;
+export(float) var DISTANCE = 115; #distancia para andar
+export(bool) var CAN_WALK = true; #pode andar?
+export(bool) var IDLE_FLIP = false; #
 
-var flip = true;
-var initial_position;
-var final_position;
-const SPEED = 0.5;
+const SPEED = 0.5; #velocidade de movimento
+const GRAVITY = 14; #gravidade para afetar o player
+var flip = true; #flipagem da imagem ao virar esquerda ou direita
+var initial_position; #posicao onde começa a andar
+var final_position; #posicao onde volta
+var alive = true; #esta vivo?
 
 
 func _ready():
-	$Sprite.play("Walk");
-	initial_position = position.x;
-	final_position = initial_position + DISTANCE;
+	if(CAN_WALK): #se pode andar, troca para a animação de andar
+		$Sprite.play("Walk");
+	initial_position = position.x; #inicializa o initial_position para ser a posição onde foi andado
+	final_position = initial_position + DISTANCE; #posicao final é a inicial + distancia
 
 
 func damage():
+	$Shape.set_deferred("disabled", true); #desativa a caixa de colisão quando o montro toma dano
+	get_node("Animation").play("Die"); #ativa a animação de morte do $Animation
+	alive = false; #seta que o montro está morto
+	CAN_WALK = false; #desativa se pode andar
+	return true;
+
+
+func removeBody():
 	self.queue_free();
 
 
 func _process(_delta):
-	if(!WALK):
-		$Sprite.play("Idle");
-		$Sprite.flip_h = true;
-		return;
+	if(!CAN_WALK):
+		if(alive):
+			$Sprite.play("Idle");
+		$Sprite.flip_h = IDLE_FLIP;
 	elif(initial_position <= final_position && flip):
 		position.x += SPEED;
-		$Sprite.flip_h = false;
+		IDLE_FLIP = false;
+		$Sprite.flip_h = IDLE_FLIP;
 		if(position.x >= final_position):
-			flip =false;
+			flip = false;
 	elif(initial_position <= position.x && !flip):
 		position.x -= SPEED;
-		$Sprite.flip_h = true;
+		IDLE_FLIP = true;
+		$Sprite.flip_h = IDLE_FLIP;
 		if(position.x <= initial_position):
 			flip =true;
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,14 +1,15 @@
 extends KinematicBody2D;
 
-export(Vector2) var RESPAWN_POSITION = Vector2(0, 0);
-export(int) var SPEED = 300;
-export(bool) var DOUBLE_JUMP = true;
-export(int) var JUMP_HEIGHT = -390;
+export(Vector2) var RESPAWN_POSITION = Vector2(0, 0); #posição de respawn
+export(int) var SPEED = 300; #velocidade de movimento
+export(bool) var DOUBLE_JUMP = true; #pulo duplo
+export(int) var JUMP_HEIGHT = -390; #tamanho do pulo
+export(int) var HEALTH_POINTS = 3;
 
-const UP = Vector2(0, -1);
-const GRAVITY = 14;
-var motion = Vector2();
-var extraJump = false;
+const UP = Vector2(0, -1); #aponta onde é emcima no x e y
+const GRAVITY = 14; #gravidade para afetar o player
+var motion = Vector2(); #movimento do Player
+var extraJump = false; #pulo duplo
 
 
 func playerMovement(leftButton, rightButton, acceleration):
@@ -45,8 +46,8 @@ func isFloatingSlowdown(acceleration): #se estiver flutuando, desaselera o "anda
 func playerJump(button): #pulo e pulo duplo
 	if(is_on_floor()): #pulo
 		extraJump = true; #reseta pulo duplo
-		if(motion.y>0):
-			motion.y=GRAVITY;
+		if(motion.y>0): #reseta gravidade minima
+			motion.y=GRAVITY; #movimento minimo para gravidade
 		if(Input.is_action_just_pressed(button)): #Pulo no chão
 			motion.y = JUMP_HEIGHT; #distancia do pulo
 	else: #se estiver voando animação de queda e pulo duplo
@@ -58,21 +59,35 @@ func playerJump(button): #pulo e pulo duplo
 			motion.y += GRAVITY; #adição da gravidade
 
 
-func playerRespawn(button, respawn_position): #evento de respawn
+func playerRespawn(button, respawn_position): #evento de respawn por controle
 	if(Input.is_action_just_pressed(button)):
 		motion = Vector2(0,14); #reseta o motion para restar a movimentação e aceleração do personagem
-		position = respawn_position; #altera o atributo hidden "position" do Player.
+		respawn(respawn_position);
+
+
+func respawn(respawn_position): #evento de respawn
+	position = respawn_position; #altera o atributo hidden "position" do Player.
 
 
 func playerCommandsHandler(acceleration): #controlador de comandos do player
-	playerMovement("ui_left", "ui_right", acceleration);
-	playerJump("ui_up");
-	playerRespawn("ui_down", RESPAWN_POSITION);
+	playerMovement("ui_left", "ui_right", acceleration); #movimento lateral
+	playerJump("ui_up"); #pulo
+	playerRespawn("ui_down", RESPAWN_POSITION); #respawn engalilhado na seta pra baixo
 
 
 func _on_Pes_body_entered(body):
-	motion.y = JUMP_HEIGHT;
-	body.damage();
+	if(body.damage()): #caso os pes entrem em contato com o monstro, executa o damage, que caso mate, da um jump extra pro user
+		motion.y = JUMP_HEIGHT; #pula
+		extraJump = true; #dá o extra jump de volta
+
+
+func _on_Damage_body_entered(_body):
+	if(HEALTH_POINTS>1):
+		print("DANO");
+		HEALTH_POINTS-=1;
+	else:
+		HEALTH_POINTS = 3;
+		respawn(RESPAWN_POSITION); 
 
 
 func _physics_process(_delta):
@@ -80,6 +95,8 @@ func _physics_process(_delta):
 	acceleration = isFloatingSlowdown(acceleration); #se estiver no ar diminui aceleração lateral
 	isOnCeilingRemoveYAcceleration(); #retira acelleração caso bata no teto
 	playerCommandsHandler(acceleration); #todos comandos do player serão por esse controlador
-	move_and_slide(motion, UP);
+	move_and_slide(motion, UP); #movimenta o Player
+
+
 
 
